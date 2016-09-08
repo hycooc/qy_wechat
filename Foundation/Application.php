1d6a5f74b92304178f367ed8612a1a4d45fd4140
宝儿哥 <baoerge123@163.com>
2016-09-08 16:52:27 +0800

    创建project 新增门面服务

---
+++ Foundation/Application.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: baoerge
+ * Date: 2016/9/8
+ * Time: 14:55
+ */
+namespace QyWecaht\Foundation;
+
+class Appliacation
+{
+
+}
---
+++ Foundation/Config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: baoerge
+ * Date: 2016/9/8
+ * Time: 14:55
+ */
+
+namespace QyWecaht\Foundation;
+
+use QyWecaht\Support\Collection;
+
+class Config extends Collection
+{
+
+}
