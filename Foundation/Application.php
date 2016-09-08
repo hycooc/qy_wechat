0639282060d6f57323ceed6fa444017ea6dc93d4
宝儿哥 <baoerge123@163.com>
2016-09-08 21:02:37 +0800

    创建project 新增门面服务

--- Foundation/Application.php
+++ Foundation/Application.php
@@ -7,7 +7,47 @@
  */
 namespace QyWecaht\Foundation;
 
-class Appliacation
+
+use Pimple\Container;
+use Symfony\Component\HttpFoundation\Request;
+
+class Appliacation extends Container
 {
+
+    protected $providers = [
+
+    ];
+
+    public function __construct($config)
+    {
+        parent::__construct();
+
+        $this['config'] = function () use ($config) {
+            return new Config($config);
+        };
+
+        //是否开启debug
+        if ($this['config']['debug']) {
+            error_reporting(E_ALL);
+        }
+
+
+    }
+
+    private function registerProviders()
+    {
+        foreach ($this->providers as $provider) {
+            $this->register(new $provider());
+        }
+
+    }
+
+    private function registerBaseProviders()
+    {
+        $this['request'] = function () {
+          return Request::createFromGlobals();
+        };
+    }
+
 
 }
